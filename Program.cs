a1a3a40badfcaa1168a326c21e792ab87e911185
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-25 17:32:32 -0500

    set up initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common_Types_Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
