cf4a92f82d446abced39cc066dbb95db7d2b340b
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-27 10:31:24 -0500

    finished dictionaries practice

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Common_Types_Dictionaries
 {
@@ -6,7 +7,44 @@ namespace Common_Types_Dictionaries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ===== Practice:  Dictionary of Words =====
+            /*
+                Create a dictionary with key value pairs to
+                represent words (key) and its definition (value)
+            */
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>() {
+
+                {"software engineer", "someone who writes software"},
+                {"family", "a collection of individuals who love each other"},
+                {"mother", "a woman who takes care of her children"}
+
+            };
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Travel", "To go away on an adventure to exerience new things");
+            wordsAndDefinitions.Add("Potty Train", "Mental exhaustion teaching a toddler to use the bathroom");
+            wordsAndDefinitions.Add("Love", "Taking care of what matters the most and doing the right thing");
+
+            /*
+                Use square brackets to get the definition of two of the
+                words and then output them to the console
+            */
+
+                Console.WriteLine(wordsAndDefinitions["Love"]);
+                Console.WriteLine(wordsAndDefinitions["Potty Train"]);
+            
+            /*
+                Below, loop over the wordsAndDefinitions dictionary to get the following output:
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+                Console.WriteLine($"The definition of {word.Key} is {word.Value}.");
+            }
+
+            // ===== Practice:  List of Dictionaries About Words =====
         }
     }
 }
